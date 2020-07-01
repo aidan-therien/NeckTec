df26c6451f0e44d8679b6819e79263a48b87f416
James Allen <jamesallen@Jamess-MacBook-Pro.local>
2020-07-01 15:17:26 -0400

    Commiting new get status route

--- server.py
+++ server.py
@@ -30,9 +30,31 @@ def add_physician_to_db(info):
 
 def init_db():
     print("connecting to database...")
-    connect("mongodb+srv://aidan:Yf3GIDiE8GzUYyZB@cluster0.lmu1g.mongodb.net"
+    connect("mongodb+srv://james:nbzKGQnRbS8nXh5E@cluster0.lmu1g.mongodb.net"
             "/NeckTecDB?retryWrites=true&w=majority")
     print("database connected.")
+
+def get_physician_status(phys_id):
+    phys_id = int(phys_id)
+    for phys in phys_db:
+        if phys["phys_id"] == phys_id:
+            neck_angles = phys['neck_angles']
+            if len(neck_angles) == 1:
+                neck_angles = neck_angles[0]
+            else:
+                neck_angles = neck_angles[-1]
+            status = phys['status']
+            timestamp = phys['timestamp']
+            if len(timestamp) == 1:
+                timestamp = timestamp[0]
+            else:
+                timestamp = timestamp[-1]
+            status_phys = {"neck_angles": neck_angles,
+                             "timestamp": timestamp}
+            return status_phys
+    return "Physician not found"
+    
+    
 
 
 @app.route("/api/new_physician", methods=["POST"])
@@ -41,11 +63,15 @@ def post_new_physician():
     physician_info = read_physician(in_dict)
     add_physician_to_db(physician_info)
     return "Successfully added physician to database.", 400
+    
+@app.route("/api/status/<phys_id>", methods=["GET"])
+def get_physician_status(phys_id):
+    return jsonify(get_physician_status(phys_id))
 
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="status.log", filemode='w',
+    logging.basicConfig(filename="../../Desktop/NeckTek/status.log", filemode='w',
                         level=logging.DEBUG)
     init_db()
     app.run()
