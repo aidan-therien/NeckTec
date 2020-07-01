22d69f7b1bb611ecc0336a5366ecea01d0bec603
aidan-therien <aidan.therien@duke.edu>
2020-07-01 15:47:58 -0400

    fixed issue with server not retrieving data

--- server.py
+++ server.py
@@ -34,27 +34,18 @@ def init_db():
             "/NeckTecDB?retryWrites=true&w=majority")
     print("database connected.")
 
-def get_physician_status(phys_id):
+
+def retrieve_physician_status(phys_id):
     phys_id = int(phys_id)
-    for phys in phys_db:
-        if phys["phys_id"] == phys_id:
-            neck_angles = phys['neck_angles']
-            if len(neck_angles) == 1:
-                neck_angles = neck_angles[0]
-            else:
-                neck_angles = neck_angles[-1]
-            status = phys['status']
-            timestamp = phys['timestamp']
-            if len(timestamp) == 1:
-                timestamp = timestamp[0]
-            else:
-                timestamp = timestamp[-1]
-            status_phys = {"neck_angles": neck_angles,
-                             "timestamp": timestamp}
-            return status_phys
-    return "Physician not found"
-    
-    
+    try:
+        temp = NewPhysician.objects.raw({"_id": phys_id}).first()
+        neck_angles = temp.neck_angles
+        timestamp = temp.timestamp
+        status_phys = {"neck_angles": neck_angles,
+                       "timestamp": timestamp}
+    except pymodm_errors.DoesNotExist:
+        return "Physician not found", 400
+    return status_phys
 
 
 @app.route("/api/new_physician", methods=["POST"])
@@ -63,15 +54,15 @@ def post_new_physician():
     physician_info = read_physician(in_dict)
     add_physician_to_db(physician_info)
     return "Successfully added physician to database.", 400
-    
+
+
 @app.route("/api/status/<phys_id>", methods=["GET"])
 def get_physician_status(phys_id):
-    return jsonify(get_physician_status(phys_id))
-
+    return jsonify(retrieve_physician_status(phys_id))
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="../../Desktop/NeckTek/status.log", filemode='w',
+    logging.basicConfig(filename="status.log", filemode='w',
                         level=logging.DEBUG)
     init_db()
     app.run()
